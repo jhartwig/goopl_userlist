4f73097cac3f0b28ec509877d187a565949e5162
Sebastian Thiele <sebastian+github@thiele.me>
2011-09-19 16:34:46 +0200

    old files by jan with .emptydir files for empty dirs

---
+++ test/unit/de/goopl/gplus/JanControllerTests.groovy
@@ -0,0 +1,16 @@
+package de.goopl.gplus
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(JanController)
+class JanControllerTests {
+
+    void testSomething() {
+
+    }
+}
